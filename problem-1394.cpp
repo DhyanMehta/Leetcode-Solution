66c85918e3f2239c415d47f642bdd68baeed1c6f
Dhyan_Mehta <dhyanm2701@gmail.com>
2025-07-06 20:56:18 +0530

    add 1394

---
+++ problem-1394.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+       unordered_map<int, int> freq;
+
+    for (int num : arr) {
+        freq[num]++;
+    }
+
+    int result = -1;
+    for (const auto& [num, count] : freq) {
+        if (num == count) {
+            result =max(result, num);
+        }
+    }
+
+    return result;
+        
+        
+    }
+};
