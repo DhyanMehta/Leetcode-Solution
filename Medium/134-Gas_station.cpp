9b4485362863d3ed892a3d41d06ad21e1bd0dab2
Dhyan Mehta <152066218+DhyanMehta@users.noreply.github.com>
2026-02-17 01:16:01 +0530

    add Medium Problem 134.Gas Station

---
+++ Medium/134-Gas_station.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+    {
+        int totalFuel = 0;
+        int currFuel = 0;
+        int start = 0;
+        for (int i = 0; i < gas.size(); i++)
+        {
+            int diff = gas[i] - cost[i];
+            totalFuel += diff;
+            currFuel += diff;
+            if (currFuel < 0)
+            {
+                start = i + 1;
+                currFuel = 0;
+            }
+        }
+        return (totalFuel >= 0) ? start : -1;
+    }
+};
